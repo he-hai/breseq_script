dadaf759e33c8f3db99ad444d4d2c85e981715e9
Hai He <haihe9898@gmail.com>
2021-07-11 13:11:22 +0200

    change array for easier comparison

--- breseq_script.sh
+++ breseq_script.sh
@@ -10,28 +10,33 @@ module add biotools/bowtie2-2.2.5 biotools/breseq-0.31.0 devel/R-3.2.2  # on aba
 # echo "Decompressing is done!"                 # you will see this prompt when done
 
 # DEFINE YOUR LIST OF SAMPLES
-declare -A ARRAY
-ARRAY["Strain_1"]="D1_"
-ARRAY["Strain_2"]="D2_"
-ARRAY["Strain_3"]="D3_"
+declare -a Samples; declare -a Labels
+Samples+=("Strain_1"); Labels+=("D1_")
+Samples+=("Strain_2"); Labels+=("D2_")
+Samples+=("Strain_3"); Labels+=("D3_")
 #...
 
 echo "Starting Breseq Assembly! (see log files)"
 # Modify the references (-r options) when necessary
-for key in ${!ARRAY[@]}; do
-	mkdir ${key}
-	breseq -j 24 -o ${key} -n ${key} -r GENOME.gbk -r PLASMID.gbk $(find ./ -type f -name ${ARRAY[$key]}*1.fq.gz) $(find ./ -type f -name ${ARRAY[$key]}*2.fq.gz) &> ${key}/log.txt &
+for i in ${!Samples[@]}; do
+	mkdir -p ${Samples[$i]}
+	breseq -j 4 -o ${Samples[$i]} -n ${Samples[$i]} -r GENOME.gbk -r PLASMID.gbk $(find ./ -type f -name ${Labels[$i]}*1.fq.gz) $(find ./ -type f -name ${Labels[$i]}*2.fq.gz) &> ${Samples[$i]}/log.txt &
 done
 
 wait
 echo "Finished assembly"
 
 echo "Starting the comparison!"
-mkdir comparison
-for key in ${!ARRAY[@]}; do 
-   cp ./${key}/output/output.gd ./comparison/${key}.gd
+mkdir -p comparison
+declare -a output=()
+for key in ${!Samples[@]}; do 
+   cp cp ./${Samples[$i]}/output/output.gd ./comparison/${Samples[$i]}.gd
+   output+=("${Samples[$i]}.gd")
 done 
 
 cd comparison
-gdtools COMPARE -o COMPARE.html -r GENOME.gbk -r PLASMID.gbk Strain_1.gd Strain_2.gd Strain_3.gd
+gdtools COMPARE -o COMPARE.html -r GENOME.gbk -r PLASMID.gbk ${output[@]}   # all samples
+# gdtools COMPARE -o COMPARE.html -r GENOME.gbk -r PLASMID.gbk ${output[@]:0:3}    # samples 0, 1, 2
 echo "Comarison is done!"
+
+exit 0
